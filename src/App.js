361e18e1c472238325e61a5ce059723b00b9add5
CharlotteLouise <150289744+Macklou@users.noreply.github.com>
2024-02-12 13:26:08 +0000

    add styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+// App.js
+
 import React from "react";
 import Weather from "./Weather";
+import "./App.css";
+import backgroundImage from "background.png";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <Weather />
     </div>
   );
